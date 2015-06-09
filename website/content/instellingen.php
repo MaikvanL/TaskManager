78556b0459934bc64f1e45ae7abd626a40145b97
GlennLouwerse <glenn_panna9_de_bomb@hotmail.com>
2015-06-09 14:41:49 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	.idea/deployment.xml
    	website/content/taaktoevoegen.php

--- website/content/instellingen.php
+++ website/content/instellingen.php
@@ -7,7 +7,7 @@ define("USER_LEVEL", 4);
 $document = new document();
 $document->open_html();
 $document->open_head();
-$document->load_metatags('Home','Description','keywords,keywords');
+$document->load_metatags('Instellingen','Description','keywords,keywords');
 $document->load_plugins();
 $document->load_stylesheets();
 $document->close_head();
@@ -29,8 +29,19 @@ include(ROOT_WEBSITE."includes/header.php");
         <hr>
         <form role="form" method="post" action="">
           <div class="form-group">
-            <label for="naam">Urent</label>
-
+            <label for="naam">TEKST</label>
+            <div class="col-md-2" style="padding-right:5px;">
+                <input type="text" class="form-control" name="voorletters" id="voorletters" placeholder="Lesuren">
+            </div>
+            <div class="col-md-2" style="padding-left:5px; padding-right:5px;">
+                <input type="text" class="form-control" name="voornaam" id="voornaam" placeholder="Voornaam">
+            </div>
+            <div class="col-md-3" style="padding-left:5px; padding-right:5px;">
+                <input type="text" class="form-control" name="tussenvoegsel" id="tussenvoegsel" placeholder="Tussenvoegsel">
+            </div>
+            <div class="col-md-3" style="padding-left:5px;">
+                <input type="text" class="form-control" name="achternaam" id="achternaam" placeholder="Achternaam">
+            </div>
 
           </div>
 
